ad760fe175b6559763daa0bd6fa159a506eb1a35
aldoBangun <punk.aldo68@gmail.com>
2022-06-21 21:37:57 +0800

    Add some middlewares and router
    - middleware: morgan, errorHandler, notFound
    - router: userRoute

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
 const PORT = process.env.PORT || 8000
-
-require('./config/database')
+const notFound = require('./routes/notFound')
+const errorHandler = require('./middleware/errorHandler')
+const routeUsers = require('./routes/users')
 
 app.use(helmet())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+app.use('/users', routeUsers)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, ()=> {
    console.log(`App is running on http://localhost:${PORT}`)
