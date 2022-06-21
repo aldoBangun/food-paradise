b91e6d1ec83fb53e24cfcd051c773d3a33a11f58
aldoBangun <punk.aldo68@gmail.com>
2022-06-21 21:32:43 +0800

    Add notFound route

---
+++ routes/notFound.js
@@ -0,0 +1,8 @@
+const asyncHandler = require("../middleware/asyncHandler")
+const ErrorResponse = require('../utils/ErrorResponse')
+
+const notFound = asyncHandler(() => {
+   throw new ErrorResponse('Request Not Found', 404)
+})
+
+module.exports = notFound
