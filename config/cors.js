ca3f8ed50f7b03553e8614beaad09b7fd519ad0a
aldoBangun <punk.aldo68@gmail.com>
2022-07-14 17:48:35 +0800

    Update cors options

---
+++ config/cors.js
@@ -0,0 +1,10 @@
+const whiteList = ['http://localhost:3000']
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1) return callback(null, true)
+    callback(new Error('Not Allowed by CORS'))
+  }
+}
+
+module.exports = corsOptions
