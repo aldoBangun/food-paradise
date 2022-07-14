9bdac0f4acec6dbe1dcc3302813f344b1c645a62
aldoBangun <punk.aldo68@gmail.com>
2022-07-14 18:04:26 +0800

    Add connection to Client

--- config/database.js
+++ config/database.js
@@ -1,13 +1,24 @@
-const { Pool } = require('pg')
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DATABASE } = process.env
+const { Pool, Client } = require('pg')
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DATABASE, DATABASE_URL } = process.env
 
-const connection = new Pool({
-  host: DB_HOST,
-  port: DB_PORT,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DATABASE
-})
+let connection
+
+if (process.env.NODE_ENV === 'production') {
+  connection = new Client({
+    connectionString: DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+  connection = new Pool({
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DATABASE
+  })
+}
 
 const connect = async () => {
   try {
