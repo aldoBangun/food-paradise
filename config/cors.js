fd26d0a63f302d7dfcac23ccbb54e9715c739b50
aldoBangun <punk.aldo68@gmail.com>
2022-07-15 07:01:46 +0800

    update cors

--- config/cors.js
+++ config/cors.js
@@ -1,10 +1,11 @@
-const whiteList = ['http://localhost:3000', 'https://food-paradise-app.herokuapp.com']
+const whiteList = ['http://localhost:3000', 'https://www.github.com', 'https://food-paradise-app.herokuapp.com', 'https://youtube.com']
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1) return callback(null, origin)
-    callback(new Error('Not Allowed by CORS'))
+const corsOptionsDelegate = function (req, callback) {
+  if (whiteList.indexOf(req.header('Origin')) !== -1) {
+    callback(null, { origin: false })
+    return
   }
+  callback(null, { origin: false })
 }
 
-module.exports = corsOptions
+module.exports = corsOptionsDelegate
