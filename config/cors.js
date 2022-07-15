a3c39fcfd0ef303bc319f997741f7e7a9d3832cd
aldoBangun <punk.aldo68@gmail.com>
2022-07-15 15:27:51 +0800

    Update whiteList

--- config/cors.js
+++ config/cors.js
@@ -1,4 +1,4 @@
-const whiteList = ['http://localhost:3000', 'https://food-paradise-app.herokuapp.com']
+const whiteList = ['https://food-paradise-react.web.app/', 'https://food-paradise-app.herokuapp.com']
 
 const corsOptionsDelegate = function (req, callback) {
   if (whiteList.indexOf(req.header('Origin')) !== -1) return callback(null, { origin: true })
