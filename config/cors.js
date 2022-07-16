0a6cd0e3545513eb7581a3cf1b06437b11cf4671
aldoBangun <punk.aldo68@gmail.com>
2022-07-16 09:51:12 +0800

    Add whilelist in development environment

--- config/cors.js
+++ config/cors.js
@@ -1,5 +1,9 @@
 const whiteList = ['https://food-paradise-react.web.app', 'https://food-paradise-app.herokuapp.com']
 
+if(process.env.NODE_ENV !== 'production') {
+  whiteList.push('http://localhost:3000')
+}
+
 const corsOptionsDelegate = function (req, callback) {
   if (whiteList.indexOf(req.header('Origin')) !== -1) return callback(null, { origin: true })
   callback(null, { origin: false })
