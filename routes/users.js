de8d67c724fa4a711879815b26f6235ef1186424
aldoBangun <punk.aldo68@gmail.com>
2022-08-09 02:55:01 +0800

    add password hashing to update user

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const router = require('express').Router()
 const { getUsers, getUser, updateUser, deleteUser } = require('../controllers/users')
 const { upload } = require('../middleware/fileUploads')
 const { tokenVerify } = require('../middleware/jwt')
+const { passwordHash } = require('../middleware/bcrypt')
 
 router.route('/').get(getUsers)
 router.route('/:id')
   .get(tokenVerify, getUser)
-  .patch(tokenVerify, upload.single('avatar'), updateUser)
+  .patch(tokenVerify, upload.single('avatar'), passwordHash, updateUser)
   .delete(tokenVerify, deleteUser)
 
 module.exports = router
