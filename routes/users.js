be3d0f09cab5222ed78ef42b9829141e08e5d727
aldoBangun <punk.aldo68@gmail.com>
2022-08-29 20:10:23 +0800

    - Bug Fix
    - Add search recipe by user functionality
    - update bcrypt hashpassword middleware
    - update create recipe controller

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,17 @@
 const router = require('express').Router()
-const { getUsers, getUser, updateUser, deleteUser } = require('../controllers/users')
+const { getUsers, getUser, updateUser, deleteUser, getUserRecipes } = require('../controllers/users')
 const { upload } = require('../middleware/fileUploads')
 const { tokenVerify } = require('../middleware/jwt')
 const { passwordHash } = require('../middleware/bcrypt')
 
 router.route('/').get(getUsers)
+
 router.route('/:id')
-  .get(tokenVerify, getUser)
+  .get(getUser)
   .patch(tokenVerify, upload.single('avatar'), passwordHash, updateUser)
   .delete(tokenVerify, deleteUser)
+  
+router.route('/:id/recipes')
+  .get(getUserRecipes)
 
 module.exports = router
