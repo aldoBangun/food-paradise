7d496e47f4d0c5b50c0d5e294a9e39b986654e84
aldoBangun <punk.aldo68@gmail.com>
2022-09-30 22:20:50 +0800

    add token verification before delete like

--- routes/likes.js
+++ routes/likes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
+const { tokenVerify } = require('../middleware/jwt')
 const { getAllLikes, deleteLike } = require('../controllers/likeRecipe')
 
 router.route('/').get(getAllLikes)
-router.route('/:likedId').delete(deleteLike)
+router.route('/:likedId').delete(tokenVerify, deleteLike)
 
 module.exports = router
