2ecddd80fc777fcfa222c09b5c33e6521c4043a6
aldoBangun <punk.aldo68@gmail.com>
2022-10-01 21:33:32 +0800

    Add likes endpoint to recipes router

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { upload } = require('../middleware/fileUploads')
+const { tokenVerify } = require('../middleware/jwt')
 const {
   getRecipes,
   getRecipeById,
@@ -8,8 +9,14 @@ const {
   updateRecipe,
   deleteRecipe
 } = require('../controllers/recipes')
-const { getCommentById, getCommentByRecipe, createComment, updateComment, deleteComment } = require('../controllers/comments')
-const { tokenVerify } = require('../middleware/jwt')
+const {
+  getCommentById,
+  getCommentByRecipe,
+  createComment,
+  updateComment,
+  deleteComment
+} = require('../controllers/comments')
+const { getLikeByRecipe, createLike, deleteLike } = require('../controllers/likeRecipe')
 const uploadFields = [{ name: 'photo' }, { name: 'videos', maxCount: 4 }]
 
 router.route('/')
@@ -23,11 +30,18 @@ router.route('/:id')
   .patch(tokenVerify, upload.fields(uploadFields), updateRecipe)
   .delete(tokenVerify, deleteRecipe)
 
-router.route('/:id/comments').get(getCommentByRecipe).post(tokenVerify, createComment)
+router.route('/:id/comments')
+  .get(getCommentByRecipe)
+  .post(tokenVerify, createComment)
 
 router.route('/:id/comments/:commentId')
   .get(getCommentById)
   .patch(tokenVerify, updateComment)
   .delete(tokenVerify, deleteComment)
+
+router.route('/:id/likes')
+  .get(getLikeByRecipe)
+  .post(tokenVerify, createLike)
+  .delete(tokenVerify, deleteLike)
 
 module.exports = router
